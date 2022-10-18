0389c214305a58f18f5cd3387a20c229a3205ae2
jalvarez <jose.alvarez@sns.it>
2022-10-18 13:20:32 +0200

    new _utils script

---
+++ src/tree/_utils.py
@@ -0,0 +1,23 @@
+from sklearn.tree import DecisionTreeClassifier
+from typing import List, Dict
+from pandas import DataFrame
+
+
+def get_current_path(subtree: DecisionTreeClassifier, ) -> List[str]:
+    # TODO
+    pass
+
+
+def get_target_probability(target_df: DataFrame, parent: str, grandparents: List[str] = None, ) -> Dict[str, float]:
+
+    target_probabilities = {}
+
+    if grandparents:
+        print("estimating conditional probabilities for {attribute}".format(attribute=parent))
+        # TODO
+
+    else:
+        print("estimating probabilities for {attribute}".format(attribute=parent))
+        # TODO
+
+    return target_probabilities
