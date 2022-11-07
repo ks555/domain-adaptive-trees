409e649f2e7f830e01dd84cd7681eee521ac17b7
jalvarez <jose.alvarez@sns.it>
2022-11-07 10:19:39 +0100

    old da functions rename to v0 and out of current architecture; added placeholder function and architecture for getting the target weights

--- src/decision_tree_classifier/decision_tree_classifier.py
+++ src/decision_tree_classifier/decision_tree_classifier.py
@@ -34,8 +34,8 @@ class DecisionTreeClassifier(object):
         # domain adaptation params
         if X_td is not None:
             self.running_da_cov_shift = True
-        if X_td is not None and y_td is not None:
-            self.running_da_con_shift = True
+        # if X_td is not None and y_td is not None:  todo later?
+        #     self.running_da_con_shift = True
         self.alpha = alpha if alpha is not None else alpha
         self.X_td = X_td if X_td is not None else X_td
         self.y_td = y_td if y_td is not None else y_td
@@ -79,7 +79,7 @@ class DecisionTreeClassifier(object):
         par_node = {'type': 'split',
                     'gain': gain, 'split_col': col, 'cutoff': cutoff, 'tot': leny, 'dist': y.value_counts(sort=False) / leny}
 
-        # the beauty of recursion (?)
+        # the beauty of recursion (?) | delete print statements later
         prev_path = self._current_path.copy()
         print(prev_path)
         # generate tree for the left hand side data
@@ -98,80 +98,52 @@ class DecisionTreeClassifier(object):
 
         return par_node, max(dleft, dright) + 1
 
-    # # todo
-    # def _store_subtree(self, par_node: Dict):
-    #     if par_node is None:
-    #         print('no data in this group')
-    #         return None
-    #     else:
-    #         # return `split` info
-    #         if par_node['type'] == 'split':
-    #             if par_node['split_col'] in self.cat:
-    #                 l_rule = '== {cutoff}'.format(cutoff=par_node['cutoff'])
-    #                 r_rule = '!= {cutoff}'.format(cutoff=par_node['cutoff'])
-    #             else:
-    #                 l_rule = '< {cutoff}'.format(cutoff=par_node['cutoff'])
-    #                 r_rule = '>= {cutoff}'.format(cutoff=par_node['cutoff'])
-    #             sub_tree = {'type': par_node['type'],
-    #                         'split_col': par_node['split_col'],
-    #                         'cutoff': par_node['cutoff'],
-    #                         'lhs': l_rule, 'rhs': r_rule}
-    #         # return `leaf` info
-    #         else:
-    #             sub_tree = {'type': par_node['type']}
-    #     return sub_tree
-
-    # todo
-    # def _get_current_path(self):
-    #     current_path = []
-    #     while self.you_are_here[0] > 0:
-    #         prev_layer = self.you_are_here[0] - 1
-
     @staticmethod
     def all_same(items):
         return all(x == items.iloc[0] for x in items)
 
+    def get_target_weights(self, current_path: List[Tuple[str, object, str]], X: DataFrame) -> Dict:
+        target_weights = {}
+        if len(current_path) > 0:
+            print('provide conditional weights based on current path')  # use current_path TODO
+        else:
+            # you're at the root node
+            print('provide the unconditional weights')  # use current_path TODO
+        # use X to build a target_weights dictionary based on the domain source (?)
+        # e.g., target_weights[c] = prob[c] where c in X.columns and prob is from the target domain info
+        #   can have target_weights[c] = None
+
+        for c in X.columns:
+            target_weights[c] = None
+
+        return target_weights
+
     def find_best_split_of_all(self, X: DataFrame, y: Series):
 
-        if len(self._you_are_here) > 0:
-            print("you're at the {side} of {col}".format(side=self._you_are_here[2], col=self._you_are_here[0]))
-
-        # you_are_here = (att: str, cutoff, side: str) todo: function handle X_td as dataframe, list, etc...
-        if self.running_da_con_shift:
-            if len(self._you_are_here) == 1:
-                # root node
-                y_td = self.y_td.copy()
-                X_td = self.X_td.copy()
-            else:
-                print(self._you_are_here)
-                y_td = self.y_td.copy()
-                X_td = self.X_td.copy()
-
-                # all other nodes
-                # current_path = self.get_current_path() todo
-
-                # if self.you_are_here[1] in self.cat:
-                #     cond = self.X_td[self.you_are_here[1]] == self.you_are_here[2]
-                # else:
-                #     cond = self.X_td[self.you_are_here[1]] < self.you_are_here[2]
-                # if self.you_are_here[3] == 'lhs':
-                #     y_td = self.y_td[cond].copy()
-                #     X_td = self.X_td[cond].copy()
-                # else:  # self.you_are_here[3] == 'rhs'
-                #     y_td = self.y_td[~cond].copy()
-                #     X_td = self.X_td[~cond].copy()
+        if len(self._you_are_here) > 0:  # delete later
+            print("you're at the {side} hs of {col}".format(side=self._you_are_here[2], col=self._you_are_here[0]))
         else:
-            y_td = None
-            X_td = None
-        ###
+            print("root node")
+
+        if self.running_da_cov_shift:
+            target_weights = self.get_target_weights(self._current_path, X)
+        else:
+            target_weights = None
 
         best_gain = 0
         best_col = None
         best_cutoff = None
         for c in X.columns:
             is_cat = c in self.cat
-            if self.running_da_con_shift:
-                gain, cur_cutoff = self.da_find_best_split_attribute(X[c], y, is_cat, X_td[c], y_td, self.alpha)
+            # domain-adaptive information gain
+            if self.running_da_cov_shift:
+                # proceed with da if we have a target weight for att c given the current path
+                if target_weights[c] is not None:
+                    gain, cur_cutoff = self.da_find_best_split_attribute(X[c], y, is_cat, self.alpha, target_weights[c])
+                # otherwise: alpha=1.0 to use only source domain info; t_weight=0.0 to keep function arch
+                else:
+                    gain, cur_cutoff = self.da_find_best_split_attribute(X[c], y, is_cat, 1.0, 0.0)
+            # standard information gain
             else:
                 gain, cur_cutoff = self.find_best_split_attribute(X[c], y, is_cat)
             if gain > best_gain:
@@ -210,7 +182,7 @@ class DecisionTreeClassifier(object):
                 best_cutoff = value
         return best_gain, best_cutoff
 
-    def da_find_best_split_attribute(self, x: Series, y: Series, is_cat: bool, x_td: Series, y_td: Series, alpha: float):
+    def da_find_best_split_attribute_v0(self, x: Series, y: Series, is_cat: bool, x_td: Series, y_td: Series, alpha: float):
         best_gain = 0
         best_cutoff = None
         if is_cat:
@@ -218,7 +190,7 @@ class DecisionTreeClassifier(object):
         else:
             values = np.sort(x.unique())
         # get entropy H(T)
-        entropy_total = self.da_info(self.y_values, y, y_td, alpha)
+        entropy_total = self.da_info_v0(self.y_values, y, y_td, alpha)
         n_tot = len(x)
         for value in values:
             cond = x == value if is_cat else x < value
@@ -230,8 +202,38 @@ class DecisionTreeClassifier(object):
             if n_right < self.min_cases:
                 continue
             # get entropy H(T|A=a)
-            entropy_left = self.da_info(self.y_values, y[cond], y_td[cond_for_td], self.alpha)     # < value
-            entropy_right = self.da_info(self.y_values, y[~cond], y_td[~cond_for_td], self.alpha)  # >= value
+            entropy_left = self.da_info_v0(self.y_values, y[cond], y_td[cond_for_td], self.alpha)     # < value
+            entropy_right = self.da_info_v0(self.y_values, y[~cond], y_td[~cond_for_td], self.alpha)  # >= value
+            left_prop = n_left / n_tot
+            right_prop = 1 - left_prop
+            # Information Gain: H(T) - H(T|A=a)
+            gain = entropy_total - left_prop * entropy_left - right_prop * entropy_right
+            if gain > best_gain:
+                best_gain = gain
+                best_cutoff = value
+        return best_gain, best_cutoff
+
+    def da_find_best_split_attribute(self, x: Series, y: Series, is_cat: bool, alpha: float, t_weight: float):
+        best_gain = 0
+        best_cutoff = None
+        if is_cat:
+            values = x.unique()
+        else:
+            values = np.sort(x.unique())
+        # get entropy H(T)
+        entropy_total = self.da_info(y, t_weight, alpha)
+        n_tot = len(x)
+        for value in values:
+            cond = x == value if is_cat else x < value
+            n_left = sum(cond)
+            if n_left < self.min_cases:
+                continue
+            n_right = n_tot - n_left
+            if n_right < self.min_cases:
+                continue
+            # get entropy H(T|A=a) todo: do we update t_weight under A=a?
+            entropy_left = self.da_info(y[cond], t_weight, alpha)     # < value
+            entropy_right = self.da_info(y[~cond], t_weight, alpha)  # >= value
             left_prop = n_left / n_tot
             right_prop = 1 - left_prop
             # Information Gain: H(T) - H(T|A=a)
@@ -252,7 +254,8 @@ class DecisionTreeClassifier(object):
         return ent
 
     @staticmethod
-    def da_info(y_values: List[int], y_source: Series, y_target: Series, alpha: float):  # todo: da_info for X_td only
+    def da_info_v0(y_values: List[int], y_source: Series, y_target: Series, alpha: float):
+        # v0: requires y_s and y_t, expecting both to be pd.Series - the ideal case
         # source domain
         vc_s = y_source.value_counts()
         tot_s = len(y_source)
@@ -273,6 +276,17 @@ class DecisionTreeClassifier(object):
             da_prop = (alpha * prop_s + (1 - alpha) * prop_t)
             if da_prop > 0.0:
                 ent -= da_prop * math.log2(da_prop)
+        return ent
+
+    @staticmethod
+    def da_info(y_source: Series, p_target: float, alpha: float):
+        vc = y_source.value_counts()
+        tot = len(y_source)
+        ent = 0
+        for v in vc:
+            prop = v / tot
+            da_w = (alpha * prop + (1 - alpha) * p_target)
+            ent -= da_w * math.log2(prop)  # todo: or da_w goes in I() too?
         return ent
 
     def predict(self, X):
