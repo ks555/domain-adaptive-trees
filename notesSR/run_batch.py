dc8faf9f065234c933ea9cb934f405ef3b999c7a
Salvatore Ruggieri <salvatore.ruggieri@unipi.it>
2023-01-08 21:36:02 +0100

    Added notesSR

---
+++ notesSR/run_batch.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Launch batch.py in parallel (on multi-core machines)
+"""
+import subprocess
+import utils
+
+# template of calling batch.py
+template = 'python batch.py {} {} subset2'
+
+processes = []
+step = 1 # smaller -> more processes
+for i in range(0, len(utils.states), step):
+    command = template.format(i, i+step)
+    process = subprocess.Popen(command, shell=True)
+    processes.append(process)
+# wait all child processes
+output = [p.wait() for p in processes]
